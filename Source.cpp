daca0944fc4a92b7ae8c829fa04e5644cbaddc2e
sky92i <48410483+sky92i@users.noreply.github.com>
2019-04-06 01:53:59 +0800

    Update data validation and add credit part

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 #include <string>
-
+#include <iomanip>
 using namespace std;
 
+void credit();
+
 int main() {
-	char user_choice="1";
-	do	// Main meun
+	int user_choice;
+
+	do	// Main menu
 	{
-
-		if (user_choice != "1" && user_choice != "2"  && user_choice != "3"  && user_choice != "4"  && user_choice != "5")	// Data validation
-		{
-			cout << "Invalid input, try again! (TESTING) \n\n";
-		}
-		cout << "A good Welcome Message will be designed by someone\n\n";
-		cout << "*** Game Menu ***\n";
-		cout << "[1] Start Game\n";
-		cout << "[2] Settings\n";
-		cout << "[3] Burger Menus\n";
-		cout << "[4] Instructions\n";
-		cout << "[5] Credits\n";
-		cout << "[6] Exit\n";
-		cout << "*****************\n";
-		cout << "Option(1 - 6) : ";
+		cout << "Welcome!\n\n"
+			<< "*** Game Menu ***\n"
+			<< "[1] Start Game\n"
+			<< "[2] Settings\n"
+			<< "[3] Burger Menus\n"
+			<< "[4] Instructions\n"
+			<< "[5] Credits\n"
+			<< "[6] Exit\n"
+			<< "*****************\n"
+			<< "Option(1 - 6) : ";
 		cin >> user_choice;
+		while (!cin) { //ignore any character
+			cin.clear();
+			cin.ignore(numeric_limits <streamsize> ::max(), '\n');
+		}
 		system("cls");
-
-	} while (user_choice != "6");
+		switch (user_choice) {
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		case 4: break;
+		case 5: credit(); break;
+		case 6: break;
+		default: cout << "Invalid input, please input again. \n\n"; break;
+		}
+	} while (user_choice != 6);
+	cout << "Good bye!" << "\n";
 	return 0;
+}
+
+void credit() {
+	string line(50, '-');
+	cout << left << setw(15) << "Name" << setw(15) << "Student ID" << setw(10) << "Class" << "Tutorial\n"
+		<< line << "\n"
+		<< left << setw(15) << "Ng Ho Tin" << setw(15) << "18188135A" << setw(10) << "203" << "Group A\n"
+		<< setw(15) << "xxxxxxxxxx" << setw(15) << "18xxxxxxA" << setw(10) << "203" << "Group A\n"
+		<< setw(15) << "xxxxxxxxxx" << setw(15) << "18xxxxxxA" << setw(10) << "203" << "Group A\n"
+		<< setw(15) << "xxxxxxxxxx" << setw(15) << "18xxxxxxA" << setw(10) << "203" << "Group A\n"
+		<< setw(15) << "xxxxxxxxxx" << setw(15) << "18xxxxxxA" << setw(10) << "203" << "Group A\n";
+	system("pause");
+	system("cls");
 }
