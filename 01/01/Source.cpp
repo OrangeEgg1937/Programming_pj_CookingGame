0ccb54af5b4fd0f4ea4fb629e6ab07d3a4535d33
18071191A 18071191A <18071191A@student.hkcc-polyu.edu.hk>
2019-04-05 21:04:54 +0800

    time and burger(hlaf)class

--- 01/01/Source.cpp
+++ 01/01/Source.cpp
@@ -5,6 +5,38 @@
 
 using namespace std;
 
+class Time
+{
+public:
+	void printTime() {
+		cout << hour << ":" << minute << ":" << second << endl;
+	}
+	void setTime(int h, int m, int s) {
+		if (validTime(h,m,s)) {
+			hour = h;
+			minute = m;
+			second = s;
+		}
+		else
+			hour = minute = second = 0;
+	}
+	int getHour() {
+		return hour;
+	}
+private:
+	int hour, minute, second;
+	bool validTime(int h, int m, int s) {
+		return (h >= 0 && h < 24 && m >= 0 && m < 60 && s >= 0 && s < 60);
+	}
+};
+
+class burger 
+{
+public:
+	burger(string, int);
+
+};
+
 int main() {
 	string user_choice = "0";
 	do	// Main meun
